4874cd1bde00c43f21289220c924f8c0c3353ede
Adrian Lodeiro <alodeiro.pro@gmail.com>
2023-02-12 21:53:45 +0100

    Users commons

---
+++ commons/commons-users/src/test/java/com/springboot/app/users/commons/CommonsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.users.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
