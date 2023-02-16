29c9f87de861624c2f9da22817a5cd2a8ba58872
Adrian Lodeiro <alodeiro.pro@gmail.com>
2023-02-16 21:21:17 +0100

    Encrypted passwords

--- users-service/src/main/resources/import.sql
+++ users-service/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `users` (username, password, enabled, name, surname, email) VALUES ('adrilope', '12345', 1, 'Adri', 'Lodeiro', 'adrilope@mail.com');
-INSERT INTO `users` (username, password, enabled, name, surname, email) VALUES ('admin', '12345', 1, 'Jhon', 'Doe', 'admin@mail.com');
+INSERT INTO `users` (username, password, enabled, name, surname, email) VALUES ('adrilope', '$2a$10$oJNmI4pc8kF9LXZFiOZ5..HRl7q0K8GbDCiqOLjbacieHY.21gxD2', 1, 'Adri', 'Lodeiro', 'adrilope@mail.com');
+INSERT INTO `users` (username, password, enabled, name, surname, email) VALUES ('admin', '$2a$10$pb5nBoAvPjfig7aHC1JQZ.JgC09LORcr8ggAPih9.MbVldgd8PISu', 1, 'Jhon', 'Doe', 'admin@mail.com');
 
 INSERT INTO `roles` (name) VALUES ('ROLE_USER');
 INSERT INTO `roles` (name) VALUES ('ROLE_ADMIN');
